d593206b866dcb1f3273a461d35dd14447cb0822
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-03-29 12:55:00 +0530

    Added SupaBase

--- hackcrux_backend/settings.py
+++ hackcrux_backend/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'hackcrux_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'db.pljgitruprxxdppbwkac.supabase.co',
+        'PORT': '5432',
     }
 }
 
